3fa9dbb6ecb3cb7705f794bad23ffa9cc4783e53
Leon Ho <Leon.lai.ho@gmail.com>
2015-10-16 15:29:16 -0700

    Created models, database not showing in database explorer

---
+++ lab/lab/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace lab.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("ProductDB") { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ lab/lab/Models/ProductDatabaseInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+namespace lab.Models
+{
+    public class ProductDatabaseInitializer: DropCreateDatabaseIfModelChanges<ProductContext>
+    {
+        protected override void Seed(ProductContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetProducts().ForEach(p => context.Product.Add(p));
+        }
+        private static List<Category> GetCategories()
+        {
+            var categories = new List<Category> {
+                new Category
+                {
+                    CategoryID = 1,
+                    CategoryName = "Media"
+                }
+            };
+            return categories;
+        }
+        private static List<Product> GetProducts()
+        {
+            var product = new List<Product> { };
+            return product;
+        }
+    }
+}
